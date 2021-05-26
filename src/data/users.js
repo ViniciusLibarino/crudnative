47501b2de115b595cf5bbcde6f3de636addd4e2f
Vinicius Libarino <viny.liba013@gmail.com>
2021-05-26 12:06:17 -0300

    dados de usuarios

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'josil@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'riamasil@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+    },
+    {
+        id: 3,
+        name: 'Julio Fragoso',
+        email: 'juliafral@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: 'rafamontee@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+    },
+]
