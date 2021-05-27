c80d141b904241f990568e6f9613717f5fe5592f
Vinicius Libarino <viny.liba013@gmail.com>
2021-05-27 01:45:12 -0300

    criando contexto

--- App.js
+++ App.js
@@ -1,52 +1,56 @@
-import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import UserList from './src/screens/UserList'
-import UserForm from './src/screens/UserForm'
-import { Button,Icon } from 'react-native-elements'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import UserList from "./src/screens/UserList";
+import UserForm from "./src/screens/UserForm";
+import { Button, Icon } from "react-native-elements";
+import { UsersProvider } from "./src/context/UserContext";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
-export default function App(props){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator 
-                initialRouteName='UserList'
-                screenOptions={screenOptions}>
-                <Stack.Screen 
-                    name='UserList'
-                    component={UserList}
-                    options={({ navigation }) => {
-                      return{
-                        title: "Lista de Usuários",
-                        headerRight: () => (
-                          <Button
-                            onPress={() => navigation.navigate("UserForm")}
-                            type="clear"
-                            icon={<Icon name="add" size={25} color="white" />}
-                          />
-                        )
-                      }
-                    }}
-                />
-                <Stack.Screen 
-                    name='UserForm'
-                    component={UserForm}
-                    options={{
-                      title:"Formulário de Usuários"
-                    }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+export default function App(props) {
+  return (
+    <UsersProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="UserList"
+          screenOptions={screenOptions}
+        >
+          <Stack.Screen
+            name="UserList"
+            component={UserList}
+            options={({ navigation }) => {
+              return {
+                title: "Lista de Usuários",
+                headerRight: () => (
+                  <Button
+                    onPress={() => navigation.navigate("UserForm")}
+                    type="clear"
+                    icon={<Icon name="add" size={25} color="white" />}
+                  />
+                ),
+              };
+            }}
+          />
+          <Stack.Screen
+            name="UserForm"
+            component={UserForm}
+            options={{
+              title: "Formulário de Usuários",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UsersProvider>
+  );
 }
 
 const screenOptions = {
   headerStyle: {
-    backgroundColor: '#f4511e'
+    backgroundColor: "#f4511e",
   },
-  headerTintColor: '#fff',
-  headerTitleStyle:{
-    fontWeight: 'bold',
-  }
-}
+  headerTintColor: "#fff",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
