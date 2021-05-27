ab5cb9c074125730e4a737422efb88c78065e5e8
Vinicius Libarino <viny.liba013@gmail.com>
2021-05-27 13:07:03 -0300

    modificando usuarios

--- src/data/users.js
+++ src/data/users.js
@@ -2,25 +2,25 @@ export default [
     {
         id: 1,
         name: 'João Silva',
-        email: 'josil@empmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+        email: 'josil@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
     },
     {
         id: 2,
         name: 'Mariana Aguiar',
-        email: 'riamasil@empmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+        email: 'riamasil@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png',
     },
     {
         id: 3,
         name: 'Julio Fragoso',
-        email: 'juliafral@empmail.com',
+        email: 'juliafral@email.com',
         avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
     },
     {
         id: 4,
         name: 'Rafael Monteiro',
-        email: 'rafamontee@empmail.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+        email: 'rafamontee@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464_960_720.png',
     },
 ]
